160101e0c36a758b456bc0e5a7df44c33c874f0f
StalinSamuelRaja <srstalinsr@gmail.com>
2024-03-25 12:45:19 +0530

    base url and spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="m-5 p-10">
-      <h1 className="font-bold text-xl pl-2">
+      <h1 className="font-bold pl-2 text-4xl font-bold mb-8 text-center text-white">
         Loading please wait
         <span className="pl-10 loading loading-spinner loading-lg"></span>
       </h1>
